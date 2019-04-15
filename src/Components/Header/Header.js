c840686b097763736d6fdc98377005a9125beddf
ruben <ruben@globatalent.com>
2019-04-15 11:21:49 +0200

    up

--- src/Components/Header/Header.js
+++ src/Components/Header/Header.js
@@ -7,7 +7,7 @@ import logo from '../../assets/img/logo.png'
 export default function Header(props) {
   let navbarBurguerClass = 'navbar-burger',
     navbarMenuClass = 'navbar-menu',
-    buttonClass = 'button is-size-6',
+    buttonClass = 'button is-size-6 has-text-weight-light',
     user;
   if (props.visible) {
     navbarBurguerClass = navbarBurguerClass + ' is-active'
