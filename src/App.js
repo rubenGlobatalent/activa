9ae548b593b69ff6db3ffcd683737e80a91b991a
Rubén Fernández Gordillo <7360184+rubfergor@users.noreply.github.com>
2019-10-14 14:11:42 +0200

    Fixed typos

--- src/App.js
+++ src/App.js
@@ -33,12 +33,12 @@ const sportsIcons = sports.list.map(sport => ({
 mapboxgl.accessToken = process.env.REACT_APP_MAPBOX_TOKEN
 
 firebase.initializeApp({
-  apiKey: process.env.REACT_APP_MAPBOX_TOKEN,
-  authDomain: process.env.REACT_APP_MAPBOX_TOKEN,
-  databaseURL: process.env.REACT_APP_MAPBOX_TOKEN,
-  projectId: process.env.REACT_APP_MAPBOX_TOKEN,
-  storageBucket: process.env.REACT_APP_MAPBOX_TOKEN,
-  messagingSenderId: process.env.REACT_APP_MAPBOX_TOKEN
+  apiKey: process.env.REACT_APP_FIRREBASE_API_KEY,
+  authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
+  projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID
 })
 
 
