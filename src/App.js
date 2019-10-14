89bc6cdf79c08d3088ed9921dad1c1ff42544f28
Rubén Fernández Gordillo <7360184+rubfergor@users.noreply.github.com>
2019-10-14 14:51:07 +0200

    Fixed MORE typos

--- src/App.js
+++ src/App.js
@@ -30,15 +30,15 @@ const sportsIcons = sports.list.map(sport => ({
   icon: reqSvgs(`./${sport.key}.png`)
 }))
 
-mapboxgl.accessToken = process.env.REACT_APP_MAPBOX_TOKEN
+mapboxgl.accessToken=process.env.REACT_APP_MAPBOX_TOKEN 
 
 firebase.initializeApp({
-  apiKey: process.env.REACT_APP_FIRREBASE_API_KEY,
+  apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
   authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
   databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
   projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
   storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID
+  messagingSenderId: 428207234830
 })
 
 
