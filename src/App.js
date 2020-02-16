2bc130ff2f61751ec0332db1853af3367f1d0502
Rubén Fernández Gordillo <7360184+rubfergor@users.noreply.github.com>
2020-02-16 22:20:26 +0100

    added measurement

--- src/App.js
+++ src/App.js
@@ -29,7 +29,8 @@ firebase.initializeApp({
   authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
   databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
   projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET
+  storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+  measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
 })
 
 const mapStateToProps = state => ({
